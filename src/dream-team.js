84c33e2987e8ef6ba5cae8aac6a316cd72c8b75d
XedDevi1 <vadim.presman@gmail.com>
2025-01-06 17:12:51 +0100

    feat: add implementation for creatDreamTeam method at the dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let teamName = [];
+
+  for (let member of members) {
+    if (typeof member === 'string') {
+      teamName.push(member.trim()[0].toUpperCase());
+    }
+  }
+
+  if (teamName.length === 0) {
+    return false;
+  }
+
+  return teamName.sort().join('');
   // remove line with error and write your code here
 }
 
