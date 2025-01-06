96a829c7cfc240e5a7b002fd7ae61b5217715aff
XedDevi1 <vadim.presman@gmail.com>
2025-01-06 16:56:42 +0100

    feat: add implementation for countCats method at the count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let count = 0;
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j] === '^^') {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
   // remove line with error and write your code here
 }
 
